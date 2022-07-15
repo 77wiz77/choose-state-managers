6f4640eca58451b9aa5bf5c41008a6e47af8ae76
Arthur <47847427+77wiz77@users.noreply.github.com>
2022-07-15 16:49:49 +1100

    install library
    add library in project - 'styled components'

--- src/layout/Header.tsx
+++ src/layout/Header.tsx
@@ -5,8 +5,8 @@ import React from 'react'
 const Header: React.FC = () => {
   return (
     <header>
-      <NavLink to='/'>Home</NavLink>
-      <NavLink to='/about'>About</NavLink>
+      <NavLink to='/'>Главная</NavLink>
+      <NavLink to='/about'>О проекте</NavLink>
       <NavLink to='/redux'>Redux</NavLink>
       <NavLink to='/mobx'>Mobx</NavLink>
       <NavLink to='/recoil'>Recoil</NavLink>
